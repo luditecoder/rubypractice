8cf20da69c12eb958707fbec475acb7a351759be
luditecoder <35017627+luditecoder@users.noreply.github.com>
2018-01-04 10:45:11 -0500

    Update and rename lesson 2- ceasers cyphr to lesson 2- ceasers cyphr.rb

---
+++ lesson 2- ceasers cyphr.rb
@@ -0,0 +1,5 @@
+#ceasers cyphur takes a phrase and shifts the letters by an amount to assign a new letter
+
+org_letters = %{abcdefghijklmnopqrstuvwxyz}
+puts "Please input your 
+for each org_letters 
