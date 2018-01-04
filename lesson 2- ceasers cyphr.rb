2dcae7ae39be7cf7ec9873b697dfa86cd7a04d46
luditecoder <35017627+luditecoder@users.noreply.github.com>
2018-01-04 10:47:33 -0500

    Update lesson 2- ceasers cyphr.rb

--- lesson 2- ceasers cyphr.rb
+++ lesson 2- ceasers cyphr.rb
@@ -1,5 +1,8 @@
 #ceasers cyphur takes a phrase and shifts the letters by an amount to assign a new letter
 
 org_letters = %{abcdefghijklmnopqrstuvwxyz}
-puts "Please input your 
+puts "Please input the sentance to be encoded"
+encoded = $stdin.gets.chomp 
+puts "Please input the key number"
+number_code = $stdin.gets.chomp
 for each org_letters 
