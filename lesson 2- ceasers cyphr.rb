db0c41a331418aa5dd9e72359cc4c1b2752f6cc8
luditecoder <35017627+luditecoder@users.noreply.github.com>
2018-01-06 21:04:21 -0500

    Update lesson 2- ceasers cyphr.rb

--- lesson 2- ceasers cyphr.rb
+++ lesson 2- ceasers cyphr.rb
@@ -17,3 +17,28 @@ for encoded_letter.each do |letter|
   decoded.push(let)
 end 
 print decoded
+
+#ceasers cyphur takes a phrase and shifts the letters by an amount to assign a new letter
+
+conversion = Hash[(:a..:z).to_a.zip((1..26).to_a)]
+print conversion
+
+puts "Please input the sentance to be encoded"
+encoded = $stdin.gets.chomp 
+puts "Please input the key number"
+number_code = $stdin.gets.chomp
+encoded_letter = encoded.split
+decoded = []
+for encoded_letter.each do |letter, value|
+  conversion[letter]
+  puts value 
+  updated_value = value + number_code
+end 
+  for updated_value.each do |letter,value|
+  conversion[value]
+  let = letter
+  decoded.push(let)
+end 
+
+puts decoded
+ 
