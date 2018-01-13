122d630be0bced04d26358598c605efe34cf6cdb
luditecoder <35017627+luditecoder@users.noreply.github.com>
2018-01-13 16:22:55 +0000

    Update Project Euler.rb

--- Project Euler.rb
+++ Project Euler.rb
@@ -1,3 +1,4 @@
+#Euler 1
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
 range = []
@@ -8,3 +9,8 @@ sum = 0
 multiples.each {|x| sum += x }
 puts "Here is the answer"
 print sum
+
+#Euler 2
+#By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+fibonacci = Hash.new{|h,k| h[k] = k < 2 ? k : h[k-1] = h[k-2]}
+puts fibonacci[5]
