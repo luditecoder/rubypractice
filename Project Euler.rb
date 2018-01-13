74bf1e4df30e76ad57e0500006d799eb9c733823
luditecoder <35017627+luditecoder@users.noreply.github.com>
2018-01-13 14:00:32 +0000

    Create Project Euler.rb

---
+++ Project Euler.rb
@@ -0,0 +1,10 @@
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#Find the sum of all the multiples of 3 or 5 below 1000.
+range = []
+0.upto(1000) {|x| range.push(x)}
+multiples = []
+range.each {|x| multiples.push(x) if  x % 3 ==0 || x % 5 == 0}
+sum = 0
+multiples.each {|x| sum += x }
+puts "Here is the answer"
+print sum
