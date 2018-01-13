36f2d8d59756df6289ffdbd35058f9a2382b538e
luditecoder <35017627+luditecoder@users.noreply.github.com>
2018-01-12 22:32:18 -0500

    Update lesson 3 - bubble sorting.rb

--- lesson 3 - bubble sorting.rb
+++ lesson 3 - bubble sorting.rb
@@ -19,6 +19,22 @@ end
  
   
  buble_sort([2, 5, 8, 1, 9, 3])
+num = [1, 5, 7, 2, 4, 8]
+
+
+
+
+def buble_sort(array)
+  num = []
+  array.each { |x| num.push(x)}
   
- 
- 
+  i = (num.length - 2)
+  
+  0.upto(i) do |x| num[x], num[x+1] = num[x + 1], num[x] if num[x] > num[x+1] 
+  print num 
+  end  
+end 
+
+ while buble_sort(num) != num.sort do buble_sort(num)
+ end 
+ #just redoes same reorginization doesn't permenantly change base array 
